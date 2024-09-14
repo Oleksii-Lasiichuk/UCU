e3352db01dc67505582a89f6c96f67165fd35ae9
Oleksii-Lasiichuk <oleks1297@gmail.com>
2024-09-14 22:42:23 +0300

    hw

--- 2/normal_distribution.py
+++ 2/normal_distribution.py
@@ -6,5 +6,5 @@ x = float(input())
 m = float(input())
 sigma = float(input())
 
-formula = (1/((2*pi*sigma**2)**0.5))* e**((-(x-m)**2)/(2*sigma**2))
+formula = (1/((2*pi*sigma**2)**0.5))* e**(-(x-m)**2/2*sigma**2)
 print(f"{formula:.10f}")
