437a4381f8ce8a0637cc1c8b2752a7c9b42513f2
Oleksii-Lasiichuk <oleks1297@gmail.com>
2024-09-14 22:39:55 +0300

    HDI HW

--- 2/cube_root.py
+++ 2/cube_root.py
@@ -4,9 +4,12 @@ b = int(input())
 c = int(input())
 d = int(input())
 
-first_formula = (-(b**3/27*a**3)) + (b*c/6*a**2) - (d/2*a)
-second_formula = (c/3*a) - (b*b/9*a**2)
+first_formula = (-((b**3)/(27*(a**3)))) + (b*c/(6*(a**2))) - (d/(2*a))
+second_formula = (c/(3*a)) - (b*b/(9*(a**2)))
 
-x = math.cbrt(first_formula + math.sqrt((first_formula**2)+(second_formula**3)) + first_formula - math.sqrt((first_formula**2)+(second_formula**3)))- b/3*a
+x = (
+    math.cbrt(first_formula + math.sqrt((first_formula**2)+(second_formula**3)))
+    + math.cbrt(first_formula - math.sqrt((first_formula**2)+(second_formula**3)))
+    - b/(3*a))
 
-print(f"{x:.2f}")
+print(f"{x = :.2f}")
