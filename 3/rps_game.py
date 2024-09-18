c07d1a18b4f7e59dbf933dd95f6f20cc9057e6b6
Oleksii-Lasiichuk <oleks1297@gmail.com>
2024-09-18 14:44:16 +0300

    add another example of solving this exersize

--- 3/rps_game.py
+++ 3/rps_game.py
@@ -1,12 +1,22 @@
+# while True:
+#     comb = input()
+#     if comb == '':
+#         break
+#     else:
+#         if comb in ["PS", "RP", "SR"]:
+#             print("False")
+#         elif comb in ["SP", "PR", "RS"]:
+#             print("True")
+#         else:
+#             print("False|False")
+    
 while True:
     comb = input()
-    if comb == '':
-        break
-    else:
-        if comb in ["PS", "RP", "SR"]:
+    match comb:
+        case "PS" | "RP" | "SR":
             print("False")
-        elif comb in ["SP", "PR", "RS"]:
+        case "SP" | "PR" | "RS":
             print("True")
-        else:
-            print("False|False")
+        case _:
+            print("False | False")
     
