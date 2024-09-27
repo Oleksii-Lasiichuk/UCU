edcec243d84c395d653825d9f9525c590e182b3e
Oleksii-Lasiichuk <oleks1297@gmail.com>
2024-09-28 01:41:39 +0300

    fixed problem with space

--- 4/alphabet.py
+++ 4/alphabet.py
@@ -6,38 +6,41 @@ number_of_letters = int(input())
 back_up_number = number_of_letters
 number_of_rows = 0
 calculator = 0
-remaining = 0
 for i in range(1, number_of_letters+1):
     if back_up_number - i > 0:
         back_up_number -= i
         number_of_rows += 1
         calculator += i
+    elif back_up_number - i == 0:
+        number_of_rows += 1
+        break
     elif back_up_number - i < 0 and i - (number_of_letters-calculator)>0:
         number_of_rows += 1
-        remaining = number_of_letters - calculator
         break
     else:
         break
+print(number_of_rows)
 
 #printing the output
 row = 1
-current_row = 1
 last_letter = 65+number_of_letters-1
 letter = 65
 
 while letter <= last_letter:
-    if number_of_letters<4:
-        print(" " * ((number_of_rows * 2)), end="")
+    if number_of_rows == 1:
+        pass
     else:
-        print(" " * ((number_of_rows * 2)-2), end="")
+        print(" " * (((number_of_rows - 1) * 2)), end="")
     for i in range(row):
         if letter > last_letter:
             break
-        if i > 0:
-            print(" ", end="")
-        print(f"{chr(letter)}", end="")
+
+        if i==0:
+            print(f"{chr(letter)}", end="")
+        elif i>0:
+            print(f" {chr(letter)}", end="")
         letter += 1
     print()
     row += 1
     number_of_rows -= 1
-
+    
